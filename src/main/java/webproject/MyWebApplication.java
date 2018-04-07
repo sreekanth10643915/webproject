1a11342f509fdce8e744b9a5362eb034569a1d42
sreekanth10643915 <sreekanth.10643915@gmail.com>
2018-04-07 17:35:54 +0530

    git fciles

---
+++ src/main/java/webproject/MyWebApplication.java
@@ -0,0 +1,16 @@
+package webproject;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPages("webproject.web");
+		registerModule(DataModel.class);
+	}
+	
+}
